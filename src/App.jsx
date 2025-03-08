4c06af3b2e61ec1ee71a3f15d51aee82d03cb964
Vasyl Martyniv <doc.people97@gmail.com>
2025-03-08 15:43:29 +0200

    finished homework 2

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import "modern-normalize/modern-normalize.css";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+
+const LOCAL_STORAGE_KEY = "state";
+
+const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setState({
+      ...state,
+      [feedbackType]: state[feedbackType] + 1,
+    });
+  };
+
+  const resetFeedback = () => {
+    setState({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
+  }, [state]);
+
+  useEffect(() => {
+    const savedState = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (savedState) {
+      setState(JSON.parse(savedState));
+    } else {
+      setState({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      });
+    }
+  }, []);
+
+  const totalFeedback = state.good + state.neutral + state.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((state.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          state={state}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+};
+
+export default App;
